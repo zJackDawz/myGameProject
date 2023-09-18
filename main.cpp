c51e54bf8bd2772664891c8f40535e72ad69cca8
zJackDawz <koonainccc@gmail.com>
2023-09-18 17:49:26 +0700

    enemy can follow
    enemy can follow and twist now
    player can attack

--- main.cpp
+++ main.cpp
@@ -4,32 +4,67 @@
 #define screenHeight 720
 
 float playerspeed = 3.0;
+float enemyspeed = 1.0;
 bool playerMoving = false;
 bool playerLeft = false;
+bool playerCanMove = true;
+bool attack = false;
 
-void input ( Vector2 *p) {
+void input (Vector2 *p, float *w) {
     Vector2 &target = *p;
-    if (IsKeyDown(KEY_RIGHT)) {
+    float &width = *w;
+    
+    if ((IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D) )) {
         target.x += playerspeed;
         playerMoving = true;
         playerLeft = false;
         }
 
-    if (IsKeyDown(KEY_LEFT)) {
+    if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) {
         target.x -= playerspeed;
         playerMoving = true;
         playerLeft = true;
         }
 
-    if (IsKeyDown(KEY_UP)) {
+    if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) {
         target.y -= playerspeed;
         playerMoving = true;
         }
 
-    if (IsKeyDown(KEY_DOWN)) {
+    if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) {
         target.y += playerspeed;
         playerMoving = true;
         }
+
+    if (playerLeft == true && width > 0) {
+            width = -width;
+        }
+    if (playerLeft == false && width < 0) {
+        width = -width;
+    }
+
+    if (IsKeyDown(KEY_SPACE)) {
+        attack = true;
+        }
+}
+
+void enemyTrac(Vector2 *p, Vector2 *t, float *w) {
+    Vector2 &enemyPos = *p;
+    Vector2 &targetPos = *t;
+    float &width = *w;
+    bool left;
+    if (targetPos.x > enemyPos.x) {enemyPos.x+=enemyspeed;left = false;}
+    if (targetPos.x < enemyPos.x) {enemyPos.x-=enemyspeed;left = true;}
+    if (targetPos.y > enemyPos.y) {enemyPos.y+=enemyspeed;}
+    if (targetPos.y < enemyPos.y) {enemyPos.y-=enemyspeed;}
+
+    if (left == true && width > 0) {
+            width = -width;
+        }
+    if (left == false && width < 0) {
+        width = -width;
+    }
+
 }
 
 int main(void)
@@ -48,18 +83,24 @@ int main(void)
 
     // Goblin
     Texture2D goblin = LoadTexture("img/charactor/goblin_run_spritesheet.png");
-    Vector2 position = { 350.0f, 280.0f };
-    Rectangle frameRec = { 0.0f, 0.0f, (float)goblin.width/6, (float)goblin.height };
+    Vector2 goblinPos = { 350.0f, 280.0f };
+    Rectangle goblinFrameRec = { 0.0f, 0.0f, (float)goblin.width/6, (float)goblin.height };
+
+    // Bush
+    Texture2D bush = LoadTexture("img/map/Bush.png");
+    Vector2 bushPos = { 450.0f, 280.0f };
+    Rectangle bushRec = { 0.0f, 0.0f, (float)bush.width, (float)bush.height };
 
     Vector2 ballPosition = { (float)screenWidth/2, (float)screenHeight/2 };
 
     int currentFrame = 0;
+    int playerCurrentFrame = 0;
     int framesCounter = 0;
     int playerFramesCounter = 0;
     int framesSpeed = 8;
 
     Camera2D camera = { 0 };
-    camera.target = (Vector2){ playerDest.x + 20.0f, playerDest.y + 20.0f };
+    camera.target = (Vector2){ playerDest.x + 20.0f, playerDest.y + 0.0f };
     camera.offset = (Vector2){ screenWidth/2.0f, screenHeight/2.0f };
     camera.zoom = 2.0f;
 
@@ -81,58 +122,70 @@ int main(void)
             if (currentFrame > 5) currentFrame = 0;
 
             // Pick Sprite
-            frameRec.x = (float)currentFrame*(float)goblin.width/6;
+            goblinFrameRec.x = (float)currentFrame*(float)goblin.width/6;
         }
 
         playerFramesCounter++;
         if (playerFramesCounter >= (60/framesSpeed))
         {
             playerFramesCounter = 0;
-            currentFrame++;
+            playerCurrentFrame++;
 
-            if (currentFrame > 5) currentFrame = 0;
+            if (playerCurrentFrame > 5) playerCurrentFrame = 0;
 
             // Pick Sprite
-            playerFrameRec.x = (float)currentFrame*(float)playerIdle.width/6;
+            playerFrameRec.x = (float)playerCurrentFrame*(float)playerIdle.width/6;
             
         }
 
-        input(&playerDest);
+        input(&playerDest, &playerFrameRec.width);
+        enemyTrac(&goblinPos, &playerDest, &goblinFrameRec.width);
 
-        if (playerMoving == true) {
+        if (playerMoving) {
             playerAnime = playerRun;
         }
         else {
             playerAnime = playerIdle;
         }
-        if (playerLeft == true && playerFrameRec.width > 0) {
-            playerFrameRec.width = -playerFrameRec.width;
-        }
-        if (playerLeft == false && playerFrameRec.width < 0) {
-            playerFrameRec.width = -playerFrameRec.width;
-        }
 
+        camera.target = (Vector2){ playerDest.x + 0, playerDest.y + 0 };
 
-        camera.target = (Vector2){ playerDest.x + 20, playerDest.y + 20 };
+        Vector2 slash = {30, 80};
+        Vector2 hand = playerDest;
+        hand.x = playerDest.x + ((float)playerAnime.width/6)/2;
+        hand.y = playerDest.y + ((float)playerAnime.height)/2;
 
-        
+        hand.y = hand.y - (slash.y/2);
+
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
 
             BeginMode2D(camera);
 
+                if (attack) {
+                    if(playerLeft) {
+                        hand.x = hand.x-slash.x;
+                        DrawRectangleV(hand, slash, MAROON);
+                    }
+                    else {
+                        DrawRectangleV(hand, slash, MAROON);
+                    }
+                }
+
                 DrawCircleV(ballPosition, 20, MAROON);
-                DrawTextureRec(goblin, frameRec, position, WHITE);
+                DrawTextureRec(bush, bushRec, bushPos, WHITE);
+                DrawTextureRec(goblin, goblinFrameRec, goblinPos, WHITE);
                 DrawTextureRec(playerAnime, playerFrameRec, playerDest, WHITE);
 
             EndMode2D();
 
-            DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
+            DrawText("move with arrow keys", 10, 10, 20, DARKGRAY);
 
         EndDrawing();
 
         playerMoving = false;
+        attack = false;
 
     }
 
